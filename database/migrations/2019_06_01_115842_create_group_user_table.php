0c2f1186d1a6507669b8371f861598590f9dd043
helderam <helder.afonso.de.morais@gmail.com>
2019-06-01 15:42:43 -0300

    Relacions ok

--- database/migrations/2019_06_01_115842_create_group_user_table.php
+++ database/migrations/2019_06_01_115842_create_group_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGroupReportsTable extends Migration
+class CreateGroupUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateGroupReportsTable extends Migration
      */
     public function up()
     {
-        Schema::create('group_reports', function (Blueprint $table) {
+        Schema::create('group_user', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
 
-            $table->unsignedBigInteger('report_id');
+            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('group_id');
 
-            $table->foreign('report_id')->references('id')->on('reports');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('group_id')->references('id')->on('groups');
         });
     }
@@ -32,6 +32,6 @@ class CreateGroupReportsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('group_reports');
+        Schema::dropIfExists('group_user');
     }
 }
--- database/migrations/2019_06_01_125246_create_group_report_table.php
+++ database/migrations/2019_06_01_125246_create_group_report_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGroupUsersTable extends Migration
+class CreateGroupReportTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateGroupUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('group_users', function (Blueprint $table) {
+        Schema::create('group_report', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
 
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('report_id');
             $table->unsignedBigInteger('group_id');
 
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('report_id')->references('id')->on('reports');
             $table->foreign('group_id')->references('id')->on('groups');
         });
     }
@@ -32,6 +32,6 @@ class CreateGroupUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('group_users');
+        Schema::dropIfExists('group_report');
     }
 }
