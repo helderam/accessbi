773ad599ecc5ad28cb4adbb96d605087a9e31baa
helderam <helder.afonso.de.morais@gmail.com>
2019-06-01 15:55:24 -0300

    Final

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,8 +36,8 @@ class HomeController extends Controller
                 select g.id, g.name, g.description, gr.report_id, 
                        r.name as report_name, r.description as r_description, r.link
                 from groups g
-                join group_users gu on gu.group_id = g.id
-                join group_reports gr on gr.group_id = gu.group_id
+                join group_user gu on gu.group_id = g.id
+                join group_report gr on gr.group_id = gu.group_id
                 join reports r on r.id = gr.report_id
                 where gu.user_id = $id
             ");
